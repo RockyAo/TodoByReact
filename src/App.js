f3df81728a9dbffaa9b37ca3534c579719c5d5bb
Rocky <aoyun@zhongchebaolian.com>
2017-01-04 10:32:44 +0800

    添加假数据

--- src/App.js
+++ src/App.js
@@ -4,28 +4,7 @@ import TodoHeader from './View/TodoHeader';
 import TodoTop from './View/TodoTop';
 import TodoList from './View/TodoList';
 
-let todos = [
-    {
-      title:'这是一条待办事项',
-      completed:false,
-      id:100
-    },
-    {
-        title:'这是一条待办事项',
-        completed:false,
-        id : 101
-    },
-    {
-        title:'这是一条待办事项',
-        completed:true,
-        id : 102
-    },
-    {
-        title:'这是一条待办事项',
-        completed:false,
-        id:103
-    }
-];
+
 
 class App extends Component {
   render() {
