42f3bea90d58d7d58ae03e8d6f1459cbd78c8721
Rocky <aoyun@zhongchebaolian.com>
2017-01-04 10:29:31 +0800

    添加常量

---
+++ src/Constants/Constans.js
@@ -0,0 +1,9 @@
+const TodoConstants = {
+    LOAD_DATA: "LOAD_DATA",
+    TOGGLE_ITEM: "TOGGLE_ITEM",
+    REMOVE_ITEM: "REMOVE_ITEM",
+    CREATE_ITEM: "CREATE_ITEM",
+    EDIT_ITEM: "EDIT_ITEM",
+}
+
+export default {...TodoConstants};
